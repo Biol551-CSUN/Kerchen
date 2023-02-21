4dbf4861cfa6b8da51f7cd132b1bf852cf7c70f5
mkerchen13 <kimalyajargarokdottir@gmail.com>
2023-02-21 14:00:21 -0800

    More notes.

--- Week_05/Scripts/2023-02-21 - Using joins.R
+++ Week_05/Scripts/2023-02-21 - Using joins.R
@@ -34,6 +34,8 @@ FullData_left <- left_join(TPCData, EnviroData_wide) %>%
 
 head(FullData_left)
 
+# Ways to get mean and variance for all values in the data set
+
 ThinkData <- FullData_left %>%
   pivot_longer(cols = E:substrate.cover,
                names_to = "Variables",
@@ -48,3 +50,29 @@ FullData_left %>%
   summarize_at(vars(E:substrate.cover), funs(mean = mean,var = var))
 
 View(FullData_left)
+
+# Creating your own tibble
+
+# Make one tibble
+T1 <- tibble(Site.ID = c("A","B","C","D"),
+             temperature = c(14.1, 16.7, 15.3, 12.8))
+
+# Make another tibble
+T2 <- tibble(Site.ID = c("A","B","D","E"),
+             pH = c(7.3, 7.8, 8.1, 7.9))
+
+T1
+T2
+
+# Using the tibbles with the different join functions
+
+left_join(T1,T2)  # joins T2 to T1. Drops E site since there was no E site in T1
+right_join(T1,T2) # joins T1 to T2. Drops C site since there was no C site in T2
+
+inner_join(T1,T2) # Only keeps data that is complete in both data sets
+
+full_join(T1,T2) # Keeps everything in both data sets
+
+semi_join(T1,T2) # Only keeps the columns that are the same in both data sets
+
+anti_join(T1,T2) #Only keeps the columns that are missing from both data sets
